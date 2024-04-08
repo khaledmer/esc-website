485977cf74ebb1222b4af7f2c62dee1caa9e813d
khaledmer <155305035+khaledmer@users.noreply.github.com>
2024-04-08 03:32:39 +0200

    Update DG.js

--- src/components/departments/DG.js
+++ src/components/departments/DG.js
@@ -21,6 +21,15 @@ function dg() {
               Discussiom Group
             </h1>
           </Col>
+                 <Col>
+           <p>A department responsible of hosting discussion groups (DGs) which are online or physical meetings 
+where people meet and get to share 
+their opinion about a given topic in English. They focus on two core skills for learning English: Listening and speaking, their main purposes are: </p>
+<p> &#9733; Promote the use of English,
+ and the sense of debate and discussion.</p>
+<p> &#9733;Express thoughts and opinions.</p>
+<p> Share, ask and answer questions.</p>
+          </Col>
           <Col
             md={5}
             style={{ paddingTop: "120px", paddingBottom: "50px" }}
